d147006ce3932ed8e6abb81fe7641d7d60c323f0
Denis Grischenko <weymoze@gmail.com>
2019-08-16 19:27:35 +0300

    delete complete LWS

--- server/controllers/deleteAllDownloads.js
+++ server/controllers/deleteAllDownloads.js
@@ -7,9 +7,7 @@ const { DOWNLOADS_PATH } = require('../config');
 
 module.exports = async (req, res) => {
   logger.info(`Deleting all downloads...`);
-  const deletedPaths = await del([`${DOWNLOADS_PATH}/**/*`]);
-  logger.info(`List of deleted files and dirs: ${deletedPaths.join('\n')}`);
-  res
-    .redirect(302, 
-      `/admin?id=${Math.floor(Math.random() * 1000)}${Date.now()}`);
+  const deletedFiles = await del([`${DOWNLOADS_PATH}/**/*`]);
+  logger.info(`List of deleted files and dirs:\n${deletedFiles.join('\n')}`);
+  res.render('deleted-files', {deletedFiles})
 }
--- server/controllers/deleteAllTorrents.js
+++ server/controllers/deleteAllTorrents.js
@@ -1,21 +1,25 @@
 const { Torrent } = require("../db");
 const logger = require('../../logger')(module.filename);
 const fs = require('fs');
+const { TORRENTS_PATH } = require('../config');
+const del = require('del');
 
 module.exports = async (req, res) => {
   logger.info(`Deleting all torrents`);
 
-  const torrents = await Torrent.find({});
-  torrents.forEach(torrent => {
-    fs.unlink(torrent.filePath, err => {
-      if(err) {
-        logger.error(`Can't delete file ${torrent.filePath}`);
-        return;
-      }
+  const deletedFiles = await del([`${TORRENTS_PATH}/**/*`]);
+  logger.info(`List of deleted files and dirs:\n${deletedFiles.join('\n')}`);
+  // const torrents = await Torrent.find({});
+  // torrents.forEach(torrent => {
+  //   fs.unlink(torrent.filePath, err => {
+  //     if(err) {
+  //       logger.error(`Can't delete file ${torrent.filePath}`);
+  //       return;
+  //     }
 
-      logger.info(`deleted ${torrent.filePath}`);
-    })
-  });
+  //     logger.info(`deleted ${torrent.filePath}`);
+  //   })
+  // });
 
   const result = await Torrent.deleteMany({});
   logger.info(`${result.deletedCount} torrents were deleted from database`);
